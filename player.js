dc72e1a55035c0a7116738f5d875e30542a7ba99
Adam Ringwood <adam@ringwood.io>
2024-06-13 09:26:01 -0500

    adjust.

--- player.js
+++ player.js
@@ -71,7 +71,7 @@ function makeMove() {
   // Use the sendMove function to send moves to the server
   gameState.systems.forEach((system) => {
     if (system.fleets.length === 1 && system.fleets[0].owner === playerName && system.fleets[0].count > 20) {
-      if (Math.random() < 0.25) {
+      if (Math.random() < 0.5) {
         let randomTarget = Math.floor(Math.random() * (gameState.systems.size - 0)) + 0;
         sendMove(system.id, randomTarget, 20)
       } else {
